1c20e32fb0241b15f4dae439ac55ffff9d6d398f
Balades <haryoholuwah1@gmail.com>
2023-01-10 14:34:23 +0100

    main

--- BaladesToken.sol
+++ BaladesToken.sol
@@ -3,7 +3,12 @@ pragma solidity ^0.8.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
 contract BaladesToken is ERC20 {
-    constructor(string memory _name, string memory _symbol) ERC20(_name,_symbol){
-        _mint(msg.sender, 1000 * 10 ** 18);
+    uint8 _decimals;
+    function decimals() public view virtual override returns (uint8) {
+        return _decimals;
+    }
+    constructor(string memory _name, string memory _symbol,uint8 decimals_) ERC20(_name,_symbol){
+        _decimals = decimals_;
+        _mint(msg.sender, 1000 * 10 ** _decimals);
     }
 }
